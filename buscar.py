290cdb5fe33ba281f175ffc40f23b7b3371de527
Thiago Gomes <tt7.thiago@gmail.com>
2021-06-20 00:55:05 -0300

    base do programa ok, adicionar funcionalidades relacionadas ao fluxo e interação com o usuário.

--- buscar.py
+++ buscar.py
@@ -3,29 +3,59 @@ from bs4 import BeautifulSoup
 import pandas as pd
 pd.options.display.float_format = '${:,.2f}'.format
 
+#get request info
+
 url = "https://ptax.bcb.gov.br/ptax_internet/consultaBoletim.do?method=consultarBoletim"
 
-bacen_taxas = requests.get(url)   
-bacen_pagina = BeautifulSoup(bacen_taxas.content,'html.parser')
-options = bacen_pagina.findAll('option')
+def prep_request():
+    bacen_taxas = requests.get(url)   
+    bacen_pagina = BeautifulSoup(bacen_taxas.content,'html.parser')
+    options = bacen_pagina.findAll('option')
 
-#base do dataframe de moedas
-ndict = {
-    'code': [] ,
-    'moeda':[]
-}
+    #base do dataframe de moedas
+    ndict = {
+        'code': [] ,
+        'moeda':[]
+    }
+    #criação do dataframe com as moedas
+    for i in options:
+        ndict['code'].append(i.attrs['value'])
+        ndict['moeda'].append(i.text)
+        #list_options.append(ndict)
+    return moedas = pd.DataFrame.from_dict(ndict)
 
-#criação do dataframe com as moedas
-for i in options:
-    ndict['code'].append(i.attrs['value'])
-    ndict['moeda'].append(i.text)
-    #list_options.append(ndict)
-moedas = pd.DataFrame.from_dict(ndict)
+moedas = prep_request()
 
-for row in moedas.iterrows():
+
+def show_options():
+#escolha as suas opções
+    for item in moedas.iterrows():
+        print (f"{item[0]}-{item[1].moeda}")
+
+def get_moeda(id):
+    try:
+        moeda_selecionada = moedas[moedas.index==id]
+    except Exception as e:
+        print (e)
+        print ('Tente Novamente')
+        moeda_selecionada = "Não Localizada"
+    return moeda_selecionada
+
+def escolha_moeda():
+    #show_options()
+    print ("Escolha a sua moeada")
+    id = input ("Apenas os Números : ")
+    resultado = get_moeda(int(id))
+    print (resultado)
+    return resultado
+
+
+#post request info
+#todo 
+    #função para o formulário
+    #função para a post request
+    #preparar os dados para exportação
     
-
-print (moedas)
 
 
 
